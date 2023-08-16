d9081a2f83a356ff36e7272e91a0fbf24e698bcb
jeffboriz <arjunarjun.k88@gmail.com>
2023-08-16 16:25:07 +0530

    rest password,email confirmation

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -148,3 +148,16 @@ MESSAGE_TAGS = {
     messages.ERROR: "danger",
     
 }
+
+# Email configuration
+from decouple import config
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'foodonline Marketplace <foodonline368@gmail.com>'
+
+
